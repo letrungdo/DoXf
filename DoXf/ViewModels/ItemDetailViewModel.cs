4a7626f4dd150e43477b5847672afe2caf7c9e00
DoLT <dolt@runsystem.net>
2020-03-04 14:11:29 +0700

    init

---
+++ DoXf/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using DoXf.Models;
+
+namespace DoXf.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
